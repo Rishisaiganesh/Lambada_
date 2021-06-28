2012806c12aecb0bc1e9c50891d92eb42cc72c8a
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-28 20:16:53 +0530

    [Rishi]Adding:GettingPersonsLessthan60

--- Lambada/Lambada/Program.cs
+++ Lambada/Lambada/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lambada
 {
@@ -11,6 +12,8 @@ namespace Lambada
             // UC1 Creating list of preson And inserting Details
             System.Collections.Generic.List<Person> listpersondetails = new List<Person>();
             Insert(listpersondetails);
+            Console.WriteLine(listpersondetails);
+            Get_person_Age_Below_60(listpersondetails);
         }
         private static void Insert(List<Person> listpersondetails)
         {
@@ -19,9 +22,17 @@ namespace Lambada
             listpersondetails.Add(new Person("54666955", "Rakesh", "main road nampalli hyderabad", 45));
             listpersondetails.Add(new Person("55211255", "Anvash", "62 Ward Balaji Nagar Nellore", 61));
             listpersondetails.Add(new Person("5212515", "Raja", "jj paradise gudur 2 ward", 62));
-            Console.WriteLine(listpersondetails.ToString());
+           
 
         }
-        
+        // UC2 get person Age Below 60
+        private static void Get_person_Age_Below_60(List<Person> listpersondetails)
+        {
+            foreach(Person person in listpersondetails.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name :" + person.Name + " \t\tAge: " + person.Age);
+            }
+
+        }
     }
 }
